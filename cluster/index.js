80cde98eae175cd23bfb2bc07b3e5cffd8570126
Liu.Jun <870395192@qq.com>
2021-06-03 19:21:26 +0800

    update

--- cluster/index.js
+++ cluster/index.js
@@ -1,28 +1,39 @@
 const cluster = require('cluster');
 const http = require('http');
 // const numCPUs = require('os').cpus().length;
-const numCPUs = 4;
+const numCPUs = 3;
 
-// 测试多进程
+// 测试多进程独立内存
 let visits = 0;
 
 if (cluster.isMaster) {
-  console.log(`主进程 ${process.pid} 正在运行`);
+  console.log(`主进程已启动 pid: ${process.pid}`);
 
   // 衍生工作进程。
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    const worker = cluster.fork();
+
+    console.log('send: hi there');
+    worker.send('hi there');
+    worker.on('message', msg => {
+      console.log(`msg: ${msg} from worker#${worker.id} pid ${worker.process.pid}`);
+    });
   }
 
   cluster.on('exit', (worker, code, signal) => {
     console.log(`工作进程 ${worker.process.pid} 已退出`);
   });
 } else {
+  console.log(`工作进程已启动 worker#${cluster.worker.id} pid: ${cluster.worker.process.pid}`);
+
+  process.on('message', (msg) => {
+    console.log(`get: ${msg}`);
+    process.send(msg);
+  });
+
   // 工作进程可以共享任何 TCP 连接。
   // 在本例子中，共享的是 HTTP 服务器。
   http.createServer((req, res) => {
-    console.log('xxxxxxxxx' + process.pid)
-    console.log(req.url);
     visits ++;
     res.writeHead(200);
     res.end(`
@@ -41,6 +52,4 @@ if (cluster.isMaster) {
       </html>
     `)
   }).listen(8000);
-
-  console.log(`工作进程 ${process.pid} 已启动`);
 }
